8f8696be6c00aca7772963e9b488b75e182e6feb
Amir J <amir.jahan@gmail.com>
2023-08-24 16:53:16 -0700

    code formatted

--- G Drive App/G Drive App/ContentView.swift
+++ G Drive App/G Drive App/ContentView.swift
@@ -1,7 +1,6 @@
 // Copyright (C) Moush group 2023
 import SwiftUI
 import GoogleSignIn
-
 import GoogleSignInSwift
 import GoogleAPIClientForRESTCore
 import GTMSessionFetcherCore
@@ -10,25 +9,19 @@ import GTMSessionFetcherCore
 
 //com.googleusercontent.apps.133653199403-mr3mut1uopve5h28vdh3iutn0i3absit
 
-
-
-struct ContentView: View
-{
+struct ContentView: View {
     @State var driveID: String?
-    
-    var body: some View
-    {
+
+    var body: some View {
         VStack {
 //            GoogleSignInButton(action: handleSignInButton)
-            Button("Sign In")
-            {
+            Button("Sign In") {
                 // when sign in is pressed, sign in to google
-                handleSignInButton ()
+                handleSignInButton()
             }
-            
-            Button("Upload")
-            {
-                if(driveID == nil){
+
+            Button("Upload") {
+                if (driveID == nil) {
                     if let url = URL(string: "https://www.googleapis.com/drive/v3/drives/\(driveID)") {
                         UIApplication.shared.open(url)
                     }
@@ -38,14 +31,13 @@ struct ContentView: View
         .onAppear {
             // when the browser pops up, retore previous sign in
             GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
-                
+
             }
         }
     }
-    
+
     // TODO: find out why this is not working
-//    func grabFileFromGDrive()
-//    {
+//    func grabFileFromGDrive() {
 //        let query = GTLRDriveQuery_FilesList.query()
 //            query.pageSize = 10 // You can adjust the page size as needed
 //            service.executeQuery(query) { (ticket, fileList, error) in
@@ -61,28 +53,28 @@ struct ContentView: View
 //                }
 //            }
 //    }
-    
+
     // this opens a new window that brings us to the google login window. Then it saves your ID
     // for future use.
     func handleSignInButton() {
         GIDSignIn.sharedInstance.signIn(withPresenting: (UIApplication.shared.windows.first?.rootViewController)!) { signInResult, error in
-                
-                guard let result = signInResult else {
-                    // Inspect error
-                    
-                    return
-                }
-                
-                dump(result)
-                
-                driveID = result.user.idToken?.tokenString
-                dump (driveID)
-            
-                // If sign in succeeded, display the app's main content View.
-                let newRootView = UIHostingController(rootView: ContentView())
-                UIApplication.shared.windows.first?.rootViewController = newRootView
+
+            guard let result = signInResult else {
+                // Inspect error
+
+                return
             }
+
+            dump(result)
+
+            driveID = result.user.idToken?.tokenString
+            dump(driveID)
+
+            // If sign in succeeded, display the app's main content View.
+            let newRootView = UIHostingController(rootView: ContentView())
+            UIApplication.shared.windows.first?.rootViewController = newRootView
         }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
--- G Drive App/G Drive App/G_Drive_AppApp.swift
+++ G Drive App/G Drive App/G_Drive_AppApp.swift
@@ -2,66 +2,53 @@ import SwiftUI
 import GoogleSignIn
 
 // no changes in your AppDelegate class
-class AppDelegate: NSObject, UIApplicationDelegate
-{
+class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool
-    {
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
-            if error != nil || user == nil
-            {
-                print ("trying to restor)")
-                
+            if error != nil || user == nil {
+                print("trying to restor)")
+
                 dump(error)
                 dump(user)
-                
-              // Show the app's signed-out state.
+
+                // Show the app's signed-out state.
             } else {
-              // Show the app's signed-in state.
+                // Show the app's signed-in state.
                 dump(user)
                 dump(user?.profile?.name)
-                
             }
-          }
-        
+        }
+
         return true
     }
-    
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool
-    {
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
         var handled: Bool
-        
+
         handled = GIDSignIn.sharedInstance.handle(url)
         if handled {
             return true
         }
-        
+
         // Handle other custom URL types.
-        
+
         // If not handled by this app, return false.
         return false
     }
 }
 
-
-
-
 @main
 struct G_Drive_AppApp: App {
-    
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
-    var body: some Scene
-    {
-        WindowGroup
-        {
+    var body: some Scene {
+        WindowGroup {
             ContentView()
-            
                 .onOpenURL { url in
-                        GIDSignIn.sharedInstance.handle(url)
-                        dump(url)
-                        }
-            
+                    GIDSignIn.sharedInstance.handle(url)
+                    dump(url)
+                }
         }
     }
 }
